6195bc4ab755f74a5d3597ef297c546cb7debee4
Jon Benson <bensojon@gmail.com>
2017-09-05 18:36:19 -0700

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+variable "name" {}
+
+resource "random_id" "random" {
+  keepers {
+    uuid = "${uuid()}"
+  }
+  byte_length = 24
+}
+
+output "random" {
+  value = "${random_id.random.hex}"
+}
+
+output "hello_world" {
+  value = "Hello, ${var.name}"
+}
